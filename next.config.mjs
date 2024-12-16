a54f90635e6a583ec2a3243463a4811ecc153a40
DucThanh <phoducthanh@gmail.com>
2024-12-16 11:35:16 +0700

    update api link

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
     BASE_URL_API:
       'https://5e1e-2401-d800-1e1-7f5f-e870-3bd5-6c00-6cc5.ngrok-free.app/api',
     HIEU_URL:
-      'https://6a62-2001-ee0-41c1-367e-d957-c702-4e1d-68af.ngrok-free.app'
+      'https://edf0-2001-ee0-41c1-367e-d957-c702-4e1d-68af.ngrok-free.app'
   }
 };
 
